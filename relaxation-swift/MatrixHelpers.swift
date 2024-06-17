6b68e26204c4e5cd97b4607cdbabd15b78fdf418
Jake <jakedves@gmail.com>
2024-06-17 17:46:08 +0100

    Sequential implementation

--- relaxation-swift/MatrixHelpers.swift
+++ relaxation-swift/MatrixHelpers.swift
@@ -41,6 +41,7 @@ func printMatrix(_ matrix: [Float]) {
         }
         print()
     }
+    print()
 }
 
 func printMatrixFromBuffer(_ matrix: MTLBuffer) {
@@ -66,4 +67,56 @@ func checkConverged(_ buffer: MTLBuffer, size: Int) -> Bool {
     }
     
     return true
+}
+
+func identity<T>(_ value: T) -> T {
+    return value
+}
+
+func relaxSequential(matrix: [Float]) {
+    let width = Int(Double(matrix.count).squareRoot())
+    var readable = matrix
+    var writable = matrix
+    var converged = Array(repeating: false, count: matrix.count)
+    
+    var iterations = 0
+    
+    var allConverged = false
+    
+    while !allConverged {
+        for i in 0..<width {
+            for j in 0..<width {
+                let index = i * width + j
+                
+                if i == 0 || j == 0 || i == width - 1 || j == width - 1 {
+                    converged[index] = true
+                    continue
+                }
+                
+                let a = readable[index + 1]
+                let b = readable[index - 1]
+                let c = readable[(i + 1) * width + j]
+                let d = readable[(i - 1) * width + j]
+                
+                let avg = (a + b + c + d) / 4.0
+                writable[index] = avg
+                
+                let previous = readable[index]
+                let difference = abs(previous - avg)
+                
+                converged[index] = difference < 0.001
+            }
+        }
+        
+        iterations += 1
+        
+        // swap pointers
+        (readable, writable) = (writable, readable)
+        
+        // check if all values are true
+        allConverged = converged.allSatisfy(identity)
+    }
+    
+    print("\(iterations) iterations")
+    printMatrix(readable)
 }
